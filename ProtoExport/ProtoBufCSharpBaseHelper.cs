4ee887c1361a6bbee3114c491a6fecbfba335ef2
Blank <wangfj11@foxmail.com>
2026-04-23 17:14:52 +0800

    refactor(ProtoExport): 提取 C# Helper 共享基类
    新增 ProtoBufCSharpBaseHelper 抽象基类，将 enum/message 生成
    逻辑集中管理。Server/Unity/Godot 子类仅声明 using 列表和
    Description 特性开关，每个子类从 ~150 行精简到 ~16 行。

---
+++ ProtoExport/ProtoBufCSharpBaseHelper.cs
@@ -0,0 +1,179 @@
+using System.Text;
+
+namespace GameFrameX.ProtoExport
+{
+    internal abstract class ProtoBufCSharpBaseHelper : IProtoGenerateHelper
+    {
+        protected abstract string[] GetUsingStatements();
+        protected abstract bool ShouldGenerateDescription { get; }
+
+        public void Run(MessageInfoList messageInfoList, string outputPath, string namespaceName = "Hotfix")
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AddTemplateHeader();
+
+            foreach (var usingStatement in GetUsingStatements())
+            {
+                sb.AppendLine(usingStatement);
+            }
+
+            sb.AppendLine();
+            sb.AppendLine($"namespace {namespaceName}");
+            sb.AppendLine("{");
+
+            foreach (var operationCodeInfo in messageInfoList.Infos)
+            {
+                if (operationCodeInfo.IsEnum)
+                {
+                    AppendEnum(sb, operationCodeInfo);
+                }
+                else
+                {
+                    AppendMessage(sb, messageInfoList, operationCodeInfo);
+                }
+            }
+
+            sb.Append("}");
+            sb.AppendLine();
+            Console.WriteLine($"Generate File:{messageInfoList.OutputPath + ".cs"}");
+            File.WriteAllText(messageInfoList.OutputPath + ".cs", sb.ToString(), Encoding.UTF8);
+        }
+
+        private void AppendEnum(StringBuilder sb, MessageInfo operationCodeInfo)
+        {
+            sb.AppendLine($"\t/// <summary>");
+            sb.AppendLine($"\t/// {operationCodeInfo.Description}");
+            sb.AppendLine($"\t/// </summary>");
+            if (ShouldGenerateDescription)
+            {
+                sb.AppendLine($"\t[System.ComponentModel.Description(\"{operationCodeInfo.Description}\")]");
+            }
+            sb.AppendLine($"\tpublic enum {operationCodeInfo.Name}");
+            sb.AppendLine("\t{");
+            for (var index = 0; index < operationCodeInfo.Fields.Count; index++)
+            {
+                var operationField = operationCodeInfo.Fields[index];
+                if (string.IsNullOrEmpty(operationField.Type))
+                {
+                    continue;
+                }
+
+                sb.AppendLine($"\t\t/// <summary>");
+                sb.AppendLine($"\t\t/// {operationField.Description}");
+                sb.AppendLine($"\t\t/// </summary>");
+                if (ShouldGenerateDescription)
+                {
+                    sb.AppendLine($"\t\t[System.ComponentModel.Description(\"{operationField.Description}\")]");
+                }
+                sb.AppendLine($"\t\t{operationField.Type} = {operationField.Members},");
+                if (index < operationCodeInfo.Fields.Count - 1)
+                {
+                    sb.AppendLine();
+                }
+            }
+
+            sb.AppendLine("\t}");
+            sb.AppendLine();
+        }
+
+        private void AppendMessage(StringBuilder sb, MessageInfoList messageInfoList, MessageInfo operationCodeInfo)
+        {
+            sb.AppendLine($"\t/// <summary>");
+            sb.AppendLine($"\t/// {operationCodeInfo.Description}");
+            sb.AppendLine($"\t/// </summary>");
+            sb.AppendLine($"\t[ProtoContract]");
+            if (ShouldGenerateDescription)
+            {
+                sb.AppendLine($"\t[System.ComponentModel.Description(\"{operationCodeInfo.Description}\")]");
+            }
+            if (string.IsNullOrEmpty(operationCodeInfo.ParentClass))
+            {
+                sb.AppendLine($"\tpublic sealed class {operationCodeInfo.Name}");
+            }
+            else
+            {
+                sb.AppendLine($"\t[MessageTypeHandler((({messageInfoList.Module}) << 16) + {operationCodeInfo.Opcode})]");
+                sb.AppendLine($"\tpublic sealed class {operationCodeInfo.Name} : MessageObject, {operationCodeInfo.ParentClass}");
+            }
+
+            sb.AppendLine("\t{");
+            for (var index = 0; index < operationCodeInfo.Fields.Count; index++)
+            {
+                var operationField = operationCodeInfo.Fields[index];
+                if (!operationField.IsValid)
+                {
+                    continue;
+                }
+
+                sb.AppendLine($"\t\t/// <summary>");
+                sb.AppendLine($"\t\t/// {operationField.Description}");
+                sb.AppendLine($"\t\t/// </summary>");
+                sb.AppendLine($"\t\t[ProtoMember({operationField.Members})]");
+                if (ShouldGenerateDescription)
+                {
+                    sb.AppendLine($"\t\t[System.ComponentModel.Description(\"{operationField.Description}\")]");
+                }
+                if (operationField.IsRepeated)
+                {
+                    sb.AppendLine($"\t\tpublic List<{operationField.Type}> {operationField.Name} {{ get; set; }} = new List<{operationField.Type}>();");
+                }
+                else
+                {
+                    string defaultValue = string.Empty;
+                    if (operationField.IsKv)
+                    {
+                        defaultValue = $" = new {operationField.Type}();";
+                        sb.AppendLine($"\t\t[ProtoMap(DisableMap = true)]");
+                    }
+
+                    sb.AppendLine($"\t\tpublic {operationField.Type} {operationField.Name} {{ get; set; }}{defaultValue}");
+                }
+
+                if (index < operationCodeInfo.Fields.Count - 1)
+                {
+                    sb.AppendLine();
+                }
+            }
+
+            if (!string.IsNullOrEmpty(operationCodeInfo.ParentClass))
+            {
+                sb.AppendLine();
+                sb.AppendLine("\t\tpublic override void Clear()");
+                sb.AppendLine("\t\t{");
+                for (var index = 0; index < operationCodeInfo.Fields.Count; index++)
+                {
+                    var operationField = operationCodeInfo.Fields[index];
+                    if (!operationField.IsValid)
+                    {
+                        continue;
+                    }
+
+                    if (operationField.IsRepeated)
+                    {
+                        sb.AppendLine($"\t\t\t{operationField.Name}.Clear();");
+                    }
+                    else
+                    {
+                        if (operationField.IsKv)
+                        {
+                            sb.AppendLine($"\t\t\t{operationField.Name}.Clear();");
+                        }
+                        else
+                        {
+                            sb.AppendLine($"\t\t\t{operationField.Name} = default;");
+                        }
+                    }
+                }
+
+                sb.AppendLine("\t\t}");
+            }
+
+            sb.AppendLine("\t}");
+            sb.AppendLine();
+        }
+
+        public virtual void Post(List<MessageInfoList> operationCodeInfo, LauncherOptions launcherOptions)
+        {
+        }
+    }
+}

418f90b8864688c7fbef9a4a774d4233dd2cbc86
Blank <wangfj11@foxmail.com>
2026-05-11 02:01:26 +0800

    refactor(proto-export): 重构 TypeScript 生成器并支持可配置 import 路径
    提取 AppendEnum、AppendMessage、AppendDescription 独立方法，
    支持通过 Init 配置 import 路径前缀和 Description 生成，
    移除重复代码和已注释的无用代码。

--- ProtoExport/ProtoBufTypeScriptHelper.cs
+++ ProtoExport/ProtoBufTypeScriptHelper.cs
@@ -1,26 +1,32 @@
-﻿using System.Text;
+using System.Text;
 
 namespace GameFrameX.ProtoExport
 {
-    /// <summary>
-    /// 生成ProtoBuf 协议文件
-    /// </summary>
     [Mode(ModeType.TypeScript)]
     internal class ProtoBufTypeScriptHelper : IProtoGenerateHelper
     {
+        private string _importPath = "../network/";
+        private bool _shouldGenerateDescription;
+
         public void Init(LauncherOptions launcherOptions)
         {
+            if (!string.IsNullOrEmpty(launcherOptions.ImportPath))
+            {
+                _importPath = launcherOptions.ImportPath;
+            }
+
+            _shouldGenerateDescription = launcherOptions.IsGenerateDescription;
         }
 
         public void Run(MessageInfoList messageInfoList, string outputPath, string namespaceName)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("import IRequestMessage from \"../network/IRequestMessage\";\n");
-            sb.Append("import IResponseMessage from \"../network/IResponseMessage\";\n");
-            sb.Append("import INotifyMessage from \"../network/INotifyMessage\";\n");
-            sb.Append("import IHeartBeatMessage from \"../network/IHeartBeatMessage\";\n");
-            sb.Append("import MessageObject from \"../network/MessageObject\";\n");
-            sb.Append("import ProtoMessageHelper from \"../network/ProtoMessageHelper\";\n");
+            sb.Append($"import IRequestMessage from \"{_importPath}IRequestMessage\";\n");
+            sb.Append($"import IResponseMessage from \"{_importPath}IResponseMessage\";\n");
+            sb.Append($"import INotifyMessage from \"{_importPath}INotifyMessage\";\n");
+            sb.Append($"import IHeartBeatMessage from \"{_importPath}IHeartBeatMessage\";\n");
+            sb.Append($"import MessageObject from \"{_importPath}MessageObject\";\n");
+            sb.Append($"import ProtoMessageHelper from \"{_importPath}ProtoMessageHelper\";\n");
             sb.Append("\n");
             sb.Append($"export namespace {messageInfoList.ModuleName} {'{'}\n");
 
@@ -28,87 +34,11 @@ namespace GameFrameX.ProtoExport
             {
                 if (operationCodeInfo.IsEnum)
                 {
-                    sb.Append($"\t/// <summary>\n");
-                    sb.Append($"\t/// {operationCodeInfo.Description}\n");
-                    sb.Append($"\t/// </summary>\n");
-                    sb.Append($"\texport enum {operationCodeInfo.Name}\n");
-                    sb.Append("\t{\n");
-                    foreach (var operationField in operationCodeInfo.Fields)
-                    {
-                        sb.Append($"\t\t/// <summary>\n");
-                        sb.Append($"\t\t/// {operationField.Description}\n");
-                        sb.Append($"\t\t/// </summary>\n");
-                        sb.Append($"\t\t{operationField.Type} = {operationField.Members}, \n");
-                    }
-
-                    sb.Append("\t}\n\n");
+                    AppendEnum(sb, operationCodeInfo);
                 }
                 else
                 {
-                    sb.Append($"\t/// <summary>\n");
-                    sb.Append($"\t/// {operationCodeInfo.Description}\n");
-                    sb.Append($"\t/// </summary>\n");
-                    // if (operationCodeInfo.IsRequest)
-                    // {
-                    //     sb.Append($"\t@RegisterRequestMessageClass({operationCodeInfo.Opcode})\n");
-                    // }
-                    // else
-                    // {
-                    //     sb.Append($"\t@RegisterResponseMessageClass({operationCodeInfo.Opcode})\n");
-                    // }
-
-                    if (string.IsNullOrEmpty(operationCodeInfo.ParentClass))
-                    {
-                        sb.Append($"\texport class {operationCodeInfo.Name} {'{'}\n");
-                    }
-                    else
-                    {
-                        // sb.AppendLine($"\t[MessageTypeHandler({operationCodeInfo.Opcode})]");
-                        sb.Append($"\texport class {operationCodeInfo.Name} extends MessageObject implements {operationCodeInfo.ParentClass} {'{'}\n");
-                        sb.Append("\n");
-
-                        sb.Append($"\t\tpublic static register(): void{'{'}\n");
-                        if (operationCodeInfo.IsRequest)
-                        {
-                            sb.Append($"\t\t\tProtoMessageHelper.registerReqMessage('{messageInfoList.ModuleName}.{operationCodeInfo.Name}', {(messageInfoList.Module << 16) + operationCodeInfo.Opcode});\n");
-                        }
-                        else
-                        {
-                            sb.Append($"\t\t\tProtoMessageHelper.registerRespMessage('{messageInfoList.ModuleName}.{operationCodeInfo.Name}', {(messageInfoList.Module << 16) + operationCodeInfo.Opcode});\n");
-                        }
-
-                        sb.Append($"\t\t{'}'}\n\n");
-
-                        sb.Append($"\t\tpublic readonly PackageName: string = '{messageInfoList.ModuleName}.{operationCodeInfo.Name}';\n");
-
-                        sb.Append("\n");
-                    }
-
-                    foreach (var operationField in operationCodeInfo.Fields)
-                    {
-                        if (!operationField.IsValid)
-                        {
-                            continue;
-                        }
-
-                        if (operationField.IsRepeated)
-                        {
-                            sb.Append($"\t\t/// <summary>\n");
-                            sb.Append($"\t\t/// {operationField.Description}\n");
-                            sb.Append($"\t\t/// </summary>\n");
-                            sb.Append($"\t\t{operationField.Name}:{TypeMapper.ToTypeScript(operationField.Type)};\n\n");
-                        }
-                        else
-                        {
-                            sb.Append($"\t\t/// <summary>\n");
-                            sb.Append($"\t\t/// {operationField.Description}\n");
-                            sb.Append($"\t\t/// </summary>\n");
-                            sb.Append($"\t\t{operationField.Name}:{TypeMapper.ToTypeScript(operationField.Type)};\n\n");
-                        }
-                    }
-
-
-                    sb.Append("\t}\n\n");
+                    AppendMessage(sb, messageInfoList, operationCodeInfo);
                 }
             }
 
@@ -117,7 +47,75 @@ namespace GameFrameX.ProtoExport
             File.WriteAllText(messageInfoList.OutputPath + ".ts", sb.ToString(), Encoding.UTF8);
         }
 
+        private void AppendDescription(StringBuilder sb, string description)
+        {
+            if (!_shouldGenerateDescription)
+            {
+                return;
+            }
 
+            sb.Append($"\t/// <summary>\n");
+            sb.Append($"\t/// {description}\n");
+            sb.Append($"\t/// </summary>\n");
+        }
+
+        private void AppendEnum(StringBuilder sb, MessageInfo operationCodeInfo)
+        {
+            AppendDescription(sb, operationCodeInfo.Description);
+            sb.Append($"\texport enum {operationCodeInfo.Name}\n");
+            sb.Append("\t{\n");
+            foreach (var operationField in operationCodeInfo.Fields)
+            {
+                AppendDescription(sb, $"\t{operationField.Description}");
+                sb.Append($"\t\t{operationField.Type} = {operationField.Members}, \n");
+            }
+
+            sb.Append("\t}\n\n");
+        }
+
+        private void AppendMessage(StringBuilder sb, MessageInfoList messageInfoList, MessageInfo operationCodeInfo)
+        {
+            AppendDescription(sb, operationCodeInfo.Description);
+
+            if (string.IsNullOrEmpty(operationCodeInfo.ParentClass))
+            {
+                sb.Append($"\texport class {operationCodeInfo.Name} {'{'}\n");
+            }
+            else
+            {
+                sb.Append($"\texport class {operationCodeInfo.Name} extends MessageObject implements {operationCodeInfo.ParentClass} {'{'}\n");
+                sb.Append("\n");
+
+                sb.Append($"\t\tpublic static register(): void{'{'}\n");
+                if (operationCodeInfo.IsRequest)
+                {
+                    sb.Append($"\t\t\tProtoMessageHelper.registerReqMessage('{messageInfoList.ModuleName}.{operationCodeInfo.Name}', {(messageInfoList.Module << 16) + operationCodeInfo.Opcode});\n");
+                }
+                else
+                {
+                    sb.Append($"\t\t\tProtoMessageHelper.registerRespMessage('{messageInfoList.ModuleName}.{operationCodeInfo.Name}', {(messageInfoList.Module << 16) + operationCodeInfo.Opcode});\n");
+                }
+
+                sb.Append($"\t\t{'}'}\n\n");
+
+                sb.Append($"\t\tpublic readonly PackageName: string = '{messageInfoList.ModuleName}.{operationCodeInfo.Name}';\n");
+
+                sb.Append("\n");
+            }
+
+            foreach (var operationField in operationCodeInfo.Fields)
+            {
+                if (!operationField.IsValid)
+                {
+                    continue;
+                }
+
+                AppendDescription(sb, $"\t{operationField.Description}");
+                sb.Append($"\t\t{operationField.Name}:{TypeMapper.ToTypeScriptFromProto(operationField.Type)};\n\n");
+            }
+
+            sb.Append("\t}\n\n");
+        }
 
         public void Post(List<MessageInfoList> operationCodeInfo, LauncherOptions launcherOptions)
         {
